9754950ad58afaeb4d30161c9bf2d1dccaa0f937
La Ngọc Hùng <154392132+langochungdev@users.noreply.github.com>
2025-07-08 20:19:21 +0700

    Delete lab6demo directory

--- lab6demo/src/main/java/poly/edu/vn/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package poly.edu.vn.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import poly.edu.vn.entity.Category;
-
-public interface CategoryDao extends JpaRepository<Category, String>{
-
-}
